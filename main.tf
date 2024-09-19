aecae86e897edd64a9ea54d1b02058bafc04869c
Erick Moore <erick.moore@gmail.com>
2024-09-19 09:47:59 -0400

    doc update and tf fix

--- main.tf
+++ main.tf
@@ -180,21 +180,20 @@ module "eks" {
 # // K8S / EKS / Load Balancer
 #
 
-
 data "aws_eks_cluster_auth" "this" {
   name = var.eks_cluster_name
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.this.token
 }
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.this.token
   }
 }
