4674ef425676bb56e3022ce6bf524f5c40a1d78e
Erick Moore <erick.moore@gmail.com>
2024-10-01 15:11:26 -0500

    changed flow log to cloud watch

--- modules/network-hub/main.tf
+++ modules/network-hub/main.tf
@@ -15,14 +15,52 @@ resource "aws_vpc" "this" {
 }
 
 resource "aws_flow_log" "this" {
-  log_destination_type = "s3"
-  log_destination      = aws_s3_bucket.this.arn
-  traffic_type         = "ALL"
-  vpc_id               = aws_vpc.this.id
-  destination_options {
-    per_hour_partition = true
+  iam_role_arn    = aws_iam_role.this.arn
+  log_destination = aws_cloudwatch_log_group.this.arn
+  traffic_type    = "ALL"
+  vpc_id          = aws_vpc.this.id
+}
+
+resource "aws_cloudwatch_log_group" "this" {
+  name = "hub-vpc-flow-log-group"
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["vpc-flow-logs.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
   }
-  tags = {}
+}
+
+resource "aws_iam_role" "this" {
+  name               = "vpc-flow-log-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams",
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "this" {
+  name   = "vpc-flow-log-role-policy"
+  role   = aws_iam_role.this.id
+  policy = data.aws_iam_policy_document.this.json
 }
 
 resource "aws_s3_bucket" "this" {
